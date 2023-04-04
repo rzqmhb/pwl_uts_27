e8640e6a0749161d4fcec254a26d4719591ebe81
Roziq <roziqmahbubi22@gmail.com>
2023-04-04 15:21:18 +0700

    migration correction

--- database/migrations/2023_04_04_064225_add_column_user.php
+++ database/migrations/2023_04_04_064225_add_column_user.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         //
         Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name');
             $table->string('nama')->after('id');
         });
     }
