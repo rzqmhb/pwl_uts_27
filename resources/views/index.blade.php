f4cca93b666a2a3abe46b633411c31949d69fdb3
Roziq <roziqmahbubi22@gmail.com>
2023-04-04 15:36:15 +0700

    index correction

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,6 +15,25 @@ Home
 @section('content')
 <div class="card">
     <div class="card-header">
+    <h3 class="card-title">Profil</h3>
+
+    <div class="card-tools">
+        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+        <i class="fas fa-minus"></i>
+        </button>
+        <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+        <i class="fas fa-times"></i>
+        </button>
+    </div>
+    </div>
+    <div class="card-body">
+        <p>Nama : {{ $user->nama }}</p>
+        <p>Email : {{ $user->email }}</p>
+    </div>
+    <!-- /.card-body -->
+</div>
+<div class="card">
+    <div class="card-header">
     <h3 class="card-title">Header</h3>
 
     <div class="card-tools">
