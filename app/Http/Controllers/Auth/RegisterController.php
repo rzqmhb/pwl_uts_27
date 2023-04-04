ca682945976bc348286f692d74aa5d6fc4cba56f
Roziq <roziqmahbubi22@gmail.com>
2023-04-04 15:19:44 +0700

    register correction

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'nama' => $data['name'],
+            'nama' => $data['nama'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
